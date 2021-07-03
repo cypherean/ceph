501ecbcbe03d12c2c5547973e8c4f4a3d4070d4c
cypherean <shreyasharma.ss305@gmail.com>
2021-07-03 19:19:41 +0530

    testing

--- src/pybind/mgr/dashboard/services/feedback.py
+++ src/pybind/mgr/dashboard/services/feedback.py
@@ -3,16 +3,30 @@
 import json
 import logging
 import os
-from qa.tasks.radosgw_admin import main
+from typing import Optional
+from redminelib import Redmine
 
 from requests.auth import AuthBase
 
-from pybind.mgr.dashboard.rest_client import RestClient
+from mgr_module import CLICommand
 
-from .. import mgr
 from ..settings import Settings
 from ..exceptions import DashboardException
 from ..rest_client import RestClient
+
+
+@CLICommand('issue create')
+def cli_create_issue(self, project_id: int, tracker_id: int, subject: str, description: Optional[str] = None, category_id: Optional[str] = None, severity: Optional[str] = None, inbuf: Optional[str] = None):
+    cephTrackerClient = CephTrackerClient()
+    response = cephTrackerClient.create_issue(
+        project_id, tracker_id, subject, description, category_id, severity)
+    return 0, str[response], ''
+
+@CLICommand('issue get')
+def cli_create_issue(self, issue_number: int):
+    cephTrackerClient = CephTrackerClient()
+    response = cephTrackerClient.get_issues(issue_number=issue_number)
+    return 0, str[response], ''
 
 class RedmineAuth(AuthBase):
     def __init__(self, access_key):
@@ -26,12 +40,13 @@ class RedmineAuth(AuthBase):
 class CephTrackerClient(RestClient):
     access_key = ''
 
+    def __init__(self):
+        super().__init__('tracker.ceph.com', None, client_name='CephTracker',
+                         ssl=True, auth=RedmineAuth(self.get_api_key()), ssl_verify=True)
+
     @staticmethod
     def get_api_key():
         try:
-            access_key = Settings.CEPH_TRACKER_API_KEY
-        except TypeError:
-            # Legacy string values.
             access_key = Settings.CEPH_TRACKER_API_KEY
         except KeyError as error:
             raise DashboardException(msg='',
@@ -40,21 +55,55 @@ class CephTrackerClient(RestClient):
 
         return access_key
 
-    def __init__(self):
-        super().__init__('https://tracker.ceph.com/issues/', 443, client_name='CephTracker',
-                         ssl=True, auth=RedmineAuth(self.get_api_key()), ssl_verify=True)
-
-    @RestClient.api_get('/{issue_number}.json', resp_structure='*')
+    @RestClient.api_get('/issues/{issue_number}.json', resp_structure='*')
     def get_issues(self, issue_number, request=None):
         response = request()
         return response
 
-    @RestClient.api_post('/', resp_structure='*')
-    def create_issue(self, project_id, tracker_id, subject, description,category_id, severity, request=None):
-        response = request({'project_id': project_id,
-                            'tracker_id': tracker_id,
-                            'subject': subject,
-                            'description': description,
-                            'category_id': category_id,
-                            'Severity': severity})
-        return response
+    @RestClient.api_post('/issues', resp_structure='*')
+    def create_issue(self, project_id, tracker_id, subject, description, category_id, severity, request=None):
+        redmine = Redmine("https://tracker.ceph.com", key=self.get_api_key())
+        project = redmine.project.get("Ceph")
+        # ceph_project_id = project.id
+        issue = redmine.issue.create(
+            project_id=project_id,
+            tracker_id=tracker_id,
+            subject=subject,
+            description=description,
+            category_id=category_id,
+            Severity=severity
+        )
+        return issue
+        # response = request({
+        #     "issue":
+        #     {'project': project_id,
+        #      'tracker': tracker_id,
+        #      'subject': subject,
+        #      'description': description,
+        #      'category': category_id,
+        #      'severity': severity}})
+        # return response
+
+
+class IssueModel:
+    project_id: str
+    tracker_id: str
+    subject: str
+    description: str
+    category_id: str
+    severity: str
+
+    def __init__(self, project_id, tracker_id, subject, description, category_id, severity):
+        self.project_id = project_id
+        self.tracker_id = tracker_id
+        self.subject = subject
+        self.description = description
+        self.category_id = category_id
+        self.severity = severity
+
+    def as_dict(self):
+        return {'project_id': self.project_id,
+                'subject': self.subject,
+                'description': self.description,
+                'category_id': self.category_id,
+                'severity': self.severity}
---
+++ src/pybind/mgr/dashboard/test.py
@@ -0,0 +1,6 @@
+# import services.feedback
+
+# if __name__ == '__main__':
+#     cephTrackerClient = CephTrackerClient()
+#     response = cephTrackerClient.create_issue('Dummy Issue','Testing from ceph tracker client')
+#     print(response)
